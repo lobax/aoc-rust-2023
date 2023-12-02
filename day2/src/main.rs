a92ee9263f581842024eaec1c3f0393401971bac
Kristian Alvarez Jörgensen <kristian.jorgensen@stryda.gg>
2023-12-02 13:31:56 +0100

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,35 +1,31 @@
 use std::{fs::read_to_string, path::PathBuf, str::from_utf8};
 
 #[derive(Default)]
-struct Round {
+struct Cubes {
     red: usize,
     green: usize,
     blue: usize,
 }
 
-fn parse_digit(bytes: &[u8]) -> usize {
-    from_utf8(bytes).unwrap().parse::<usize>().unwrap()
-}
-
 fn main() {
-    let res_1 = part1(PathBuf::from("input.txt"));
-    println!("{}", res_1)
+    let input = PathBuf::from("input.txt");
+    let res_1 = part1(&input);
+    println!("{}", res_1);
+    let res_2 = part2(&input);
+    println!("{}", res_2);
 }
 
-
-fn part1(input: PathBuf) -> usize {
+fn part1(input: &PathBuf) -> usize {
     let mut sum: usize = 0;
     for line in read_to_string(input).unwrap().lines() {
         let bytes = line.as_bytes();
         let (game_id, game_bytes) = strip_game_id(bytes);
         let rounds = strip_rounds(game_bytes);
-        let possible = rounds.iter().all(|round| {
-            match round {
-                round if round.red      > 12 => false,
-                round if round.green    > 13 => false,
-                round if round.blue     > 14 => false,
-                _                                    => true
-            }
+        let possible = rounds.iter().all(|round| match round {
+            round if round.red > 12 => false,
+            round if round.green > 13 => false,
+            round if round.blue > 14 => false,
+            _ => true,
         });
         if possible {
             sum += game_id;
@@ -38,33 +34,73 @@ fn part1(input: PathBuf) -> usize {
     sum
 }
 
+fn part2(input: &PathBuf) -> usize {
+    let mut sum: usize = 0;
+    for line in read_to_string(input).unwrap().lines() {
+        let bytes = line.as_bytes();
+        let (_, game_bytes) = strip_game_id(bytes);
+        let rounds = strip_rounds(game_bytes);
+        let mut min_bag = Cubes::default();
+        rounds.iter().for_each(|round| {
+            if round.red > min_bag.red {
+                min_bag = Cubes {
+                    red: round.red.clone(),
+                    ..min_bag
+                }
+            }
+            if round.green > min_bag.green {
+                min_bag = Cubes {
+                    green: round.green.clone(),
+                    ..min_bag
+                }
+            }
+            if round.blue > min_bag.blue {
+                min_bag = Cubes {
+                    blue: round.blue.clone(),
+                    ..min_bag
+                }
+            }
+        });
+        sum += min_bag.red * min_bag.blue * min_bag.green;
+    }
+    sum
+}
+
+fn parse_digit(bytes: &[u8]) -> usize {
+    from_utf8(bytes).unwrap().parse::<usize>().unwrap()
+}
+
 fn strip_game_id(bytes: &[u8]) -> (usize, &[u8]) {
     match bytes {
-        [b'G', b'a', b'm', b'e', b' ', byte, b':', tail @ ..] => (parse_digit(&[byte.clone()]), tail),
-        [b'G', b'a', b'm', b'e', b' ', byte_1, byte_2, b':', tail @ ..] => 
-            (parse_digit(&[byte_1.clone(), byte_2.clone()]), tail),
-        [b'G', b'a', b'm', b'e', b' ', byte_1, byte_2, byte_3, b':', tail @ ..] => 
-            (parse_digit(&[byte_1.clone(), byte_2.clone(), byte_3.clone()]), tail),
-        _ => panic!("Unable to fetch game id")
+        [b'G', b'a', b'm', b'e', b' ', byte, b':', tail @ ..] => {
+            (parse_digit(&[byte.clone()]), tail)
+        }
+        [b'G', b'a', b'm', b'e', b' ', byte_1, byte_2, b':', tail @ ..] => {
+            (parse_digit(&[byte_1.clone(), byte_2.clone()]), tail)
+        }
+        [b'G', b'a', b'm', b'e', b' ', byte_1, byte_2, byte_3, b':', tail @ ..] => (
+            parse_digit(&[byte_1.clone(), byte_2.clone(), byte_3.clone()]),
+            tail,
+        ),
+        _ => panic!("Unable to fetch game id"),
     }
 }
 
-
-fn strip_rounds(bytes: &[u8]) -> Vec<Round> {
+fn strip_rounds(bytes: &[u8]) -> Vec<Cubes> {
     let mut rounds = Vec::new();
     let mut remaining = bytes;
-    let mut round = Round::default();
+    let mut round = Cubes::default();
     let mut size: usize = 0;
     loop {
         match remaining {
             [] => {
                 rounds.push(round);
-                break
-            },
+                break;
+            }
             [b';', tail @ ..] => {
                 remaining = tail;
                 rounds.push(round);
-                round = Round::default();
+                round = Cubes::default();
             }
             [b',', tail @ ..] => {
                 remaining = tail;
@@ -80,45 +116,47 @@ fn strip_rounds(bytes: &[u8]) -> Vec<Round> {
 
             [b'r', b'e', b'd', tail @ ..] => {
                 remaining = tail;
-                round = Round {
+                round = Cubes {
                     red: size.clone(),
                     ..round
                 }
-
             }
             [b'b', b'l', b'u', b'e', tail @ ..] => {
                 remaining = tail;
-                round = Round {
+                round = Cubes {
                     blue: size.clone(),
                     ..round
                 }
-
             }
             [b'g', b'r', b'e', b'e', b'n', tail @ ..] => {
                 remaining = tail;
-                round = Round {
+                round = Cubes {
                     green: size.clone(),
                     ..round
                 }
-
             }
-            _ => panic!("Unknown symbol!")
+            _ => panic!("Unknown symbol!"),
         }
     }
     rounds
 }
 
-
 #[cfg(test)]
 mod test {
     use std::path::PathBuf;
 
-    use crate::part1;
+    use crate::{part1, part2};
 
     #[test]
     fn problem_1() {
         let path = PathBuf::from("example.txt");
-        let sum = part1(path);
+        let sum = part1(&path);
         assert_eq!(8, sum)
+    }
+    #[test]
+    fn problem_2() {
+        let path = PathBuf::from("example.txt");
+        let sum = part2(&path);
+        assert_eq!(2286, sum)
     }
 }
